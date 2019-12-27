d8706f755e109de02e8ba91c18552d3821140d7d
Graham Still <gstill@uw.edu>
2019-12-27 16:23:21 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['Titillium\\ Web', 'sans-serif'],
+        body: ['Open\\ Sans', 'sans-serif']
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
