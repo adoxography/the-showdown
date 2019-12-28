87db401ca831a23464b469601c0574ff555e59df
Graham Still <gstill@uw.edu>
2019-12-27 21:30:46 -0600

    Adjust header tracking

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import { profiles } from '../mock';
 
 const About = () => (
   <section className="mt-4 pb-1 xl:mt-8 mx-4 md:mx-12 xl:mx-32">
-    <h1 className="mb-6 font-display uppercase text-3xl tracking-narrow font-thin text-center">Our staff</h1>
+    <h1 className="mb-6 font-display uppercase text-3xl tracking-tight font-thin text-center">Our staff</h1>
 
     {profiles.map((profile, i) => (
       <Profile key={i} person={profile} />
