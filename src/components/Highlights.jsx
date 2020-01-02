74488ac48711717a277c4d3c4c23cfe7d77703f8
Graham Still <gstill@uw.edu>
2020-01-01 21:52:10 -0600

    Style embedded player

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -40,7 +40,9 @@ const Preview = ({ video, onClick }) => (
 
 const Player = ({ video, onClickOutside }) => (
   <div className="w-full h-full fixed top-0 left-0 flex justify-center items-center z-50" onClick={onClickOutside} style={{ backgroundColor: '#000000a0' }}>
-    <ReactPlayer url={video.url} playing controls />
+    <div className="rounded-lg overflow-hidden shadow-2xl">
+      <ReactPlayer url={video.url} playing controls />
+    </div>
   </div>
 );
 
