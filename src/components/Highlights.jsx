9c88215893736848df457210c0c48b098175acdc
Graham Still <gstill@uw.edu>
2020-01-01 21:48:18 -0600

    Add embedded player

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 import { getPlaylistItems, playlists } from '../apis/youtube';
 import svg from '../svg';
@@ -15,11 +16,11 @@ const Loading = () => (
 /**
  * Displays a list of videos
  */
-const Videos = ({ videos }) => (
+const Videos = ({ videos, onClick }) => (
   <div className="flex flex-wrap justify-center lg:mt-4">
     {videos.map((video, i) => (
       <div key={i} className="m-2">
-        <Preview key={i} video={video} />
+        <Preview key={i} video={video} onClick={onClick} />
       </div>
     ))}
   </div>
@@ -28,13 +29,19 @@ const Videos = ({ videos }) => (
 /**
  * A preview thumbnail for a video
  */
-const Preview = ({ video }) => (
+const Preview = ({ video, onClick }) => (
   <article className="relative">
     <img src={video.thumbnail} alt={video.title} className="object-cover rounded-lg w-full h-full" />
-    <a href={video.url} className="block absolute z-10 top-0 w-full h-full text-white p-2 rounded-lg bg-gray-900 opacity-0 hover:opacity-75">
+    <a onClick={() => onClick(video)} className="block absolute z-10 top-0 w-full h-full text-white p-2 rounded-lg bg-gray-900 opacity-0 hover:opacity-75">
       <h1 className="text-lg font-semibold font-display ml-2">{video.title}</h1>
     </a>
   </article>
+);
+
+const Player = ({ video, onClickOutside }) => (
+  <div className="w-full h-full fixed top-0 left-0 flex justify-center items-center z-50" onClick={onClickOutside} style={{ backgroundColor: '#000000a0' }}>
+    <ReactPlayer url={video.url} playing controls />
+  </div>
 );
 
 class Highlights extends React.Component {
@@ -44,7 +51,8 @@ class Highlights extends React.Component {
     this.state = {
       playlist: playlists[0],
       videosLoaded: false,
-      videos: []
+      videos: [],
+      activeVideo: null
     };
   }
 
@@ -57,12 +65,22 @@ class Highlights extends React.Component {
     });
   }
 
+  handleClick = video => {
+    this.setState(() => ({ activeVideo: video }));
+  }
+
+  handleClickOutside = () => {
+    this.setState(() => ({ activeVideo: null }));
+  }
+
   render() {
     const { playlist, videos, videosLoaded } = this.state;
 
     return (
       <section>
-        {videosLoaded ? <Videos videos={videos} />: <Loading /> }
+        {videosLoaded ? <Videos videos={videos} onClick={this.handleClick} />: <Loading /> }
+
+        {this.state.activeVideo && <Player video={this.state.activeVideo} onClickOutside={this.handleClickOutside} />}
 
         <div className="flex justify-center mt-8 pb-4">
           <a href={`https://www.youtube.com/playlist?list=${playlist.id}`} target="_blank" className="border border-yellow-200 text-yellow-200 hover:bg-gray-900 text-center rounded px-3 py-2">See more</a>
