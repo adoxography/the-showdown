91c12ba590b04b954aedb2ab55b1a7e9bcb07a60
Graham Still <gstill@uw.edu>
2020-01-03 16:21:59 -0600

    Constrain video player width

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -59,7 +59,7 @@ const Preview = ({ video, onClick }) => (
 const Player = ({ video, onClickOutside }) => (
   <div className="w-full h-full fixed top-0 left-0 flex justify-center items-center z-50" onClick={onClickOutside} style={{ backgroundColor: '#000000a0' }}>
     <div className="rounded-lg overflow-hidden shadow-2xl">
-      <ReactPlayer url={video.url} playing controls />
+      <ReactPlayer url={video.url} playing controls width="100vw" />
     </div>
   </div>
 );
