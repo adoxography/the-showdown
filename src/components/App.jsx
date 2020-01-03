a19cace9ea26df81a5674e2005e5965142e77109
Graham Still <gstill@uw.edu>
2020-01-03 16:50:43 -0600

    Dynamically import main components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,45 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
 } from 'react-router-dom';
 
 import Header from './Header';
 import Navbar from './Navbar';
-import About from './About';
-import Home from './Home';
-import Highlights from './Highlights';
-import ComingSoon from './ComingSoon';
 import Footer from './Footer';
+
+const Home = lazy(() => import('./Home'));
+const About = lazy(() => import('./About'));
+const Highlights = lazy(() => import('./Highlights'));
+const ComingSoon = lazy(() => import('./ComingSoon'));
 
 const App = () => {
   return (
     <Router>
-      <React.Fragment>
         <Header />
         <Navbar />
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
+        <Suspense fallback={<div className="text-center text-gray-700">Loading...</div>}>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
 
-          <Route
-            path="/highlights/:playlist"
-            component={props => <Highlights playlist={props.match.params.playlist} key={props.location.pathname} />}
-          />
+            <Route
+              path="/highlights/:playlist"
+              component={props => <Highlights playlist={props.match.params.playlist} key={props.location.pathname} />}
+            />
 
-          <Route path="/shop">
-            <ComingSoon />
-          </Route>
+            <Route path="/shop">
+              <ComingSoon />
+            </Route>
 
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Suspense>
         <Footer />
-      </React.Fragment>
     </Router>
   );
 };
