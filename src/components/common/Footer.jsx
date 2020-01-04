21911772a3feca2643fbc678677dd0c7527b9c2f
Graham Still <gstill@uw.edu>
2020-01-04 01:16:01 -0600

    Update footer links

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <footer className="bg-gray-800 absolute bottom-0 w-full px-4 xl:px-16 h-16 pt-2 xl:pt-4 flex justify-between">
@@ -9,13 +10,11 @@ const Footer = () => (
     </div>
 
     <div className="hidden md:block text-sm text-gray-300">
-      <a href="#" className="text-gray-300 hover:text-white">Home</a>
+      <Link to="/" className="text-gray-300 hover:text-white">Home</Link>
       <span className="px-1">&bull;</span>
-      <a href="#" className="text-gray-300 hover:text-white">About</a>
+      <Link to="/about" className="text-gray-300 hover:text-white">About</Link>
       <span className="px-1">&bull;</span>
-      <a href="#" className="text-gray-300 hover:text-white">Highlights</a>
-      <span className="px-1">&bull;</span>
-      <a href="#" className="text-gray-300 hover:text-white">Shop</a>
+      <Link to="/shop" className="text-gray-300 hover:text-white">Shop</Link>
     </div>
   </footer>
 );
