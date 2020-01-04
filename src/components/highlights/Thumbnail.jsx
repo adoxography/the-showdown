5c85118d863a5b7b0a1092e7463814a5f456b3c3
Graham Still <gstill@uw.edu>
2020-01-03 23:47:50 -0600

    Use pointer cursor over thumbnails

--- src/components/highlights/Thumbnail.jsx
+++ src/components/highlights/Thumbnail.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { Video } from '../../apis/youtube';
 
 const Thumbnail = ({ video, onClick }) => (
-  <article className="relative">
+  <article className="relative cursor-pointer">
     <img src={video.thumbnail} alt={video.title} className="object-cover rounded-lg w-full h-full" />
     <a onClick={() => onClick(video)} className="block absolute z-10 top-0 w-full h-full text-white p-2 rounded-lg bg-gray-900 opacity-0 hover:opacity-75">
       <h1 className="text-lg font-semibold font-display ml-2">{video.title}</h1>
