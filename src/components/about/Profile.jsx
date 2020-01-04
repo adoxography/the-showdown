0a0e63ba1bb633119e1321630e20f72dba31e7c0
Graham Still <gstill@uw.edu>
2020-01-03 23:13:47 -0600

    Add PropTypes for Profile

--- src/components/about/Profile.jsx
+++ src/components/about/Profile.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Profile = props => {
   const { name, handle, avatar, bio } = props.person;
 
   return (
     <div className="mb-12 md:flex">
-      <img src={avatar} alt={name} className="w-48 h-48 m-auto md:m-0 object-cover rounded-full shadow-lg" />
+
+      <div className="w-48 h-48 flex-shrink-0 m-auto md:m-0 rounded-full shadow-lg bg-gray-800 text-gray-400 flex justify-center items-center">
+        {avatar ? <img src={avatar} className="object-cover w-48 h-48 rounded-full" /> : 'Image coming soon'}
+      </div>
+
       <article className="md:ml-6 md:mt-2">
         <h1 className="ml-4 md:m-0 text-yellow-600 font-display font-semibold uppercase leading-none">
           {name}
         </h1>
-        <a href={`https://twitter.com/${handle}`} className="ml-4 md:m-0 text-sm text-gray-400 hover:text-gray-200">
-          @{handle}
-        </a>
-        {bio.map((line, i) => (
+
+        {handle && (
+          <a href={`https://twitter.com/${handle}`} className="ml-4 md:m-0 text-sm text-gray-400 hover:text-gray-200">
+            @{handle}
+          </a>
+        )}
+
+        {!bio ? <p className="mt-2 text-center">Bio coming soon</p> : bio.map((line, i) => (
           <p key={i} className="mt-2">
             {line}
           </p>
@@ -21,6 +30,15 @@ const Profile = props => {
       </article>
     </div>
   );
+};
+
+Profile.propTypes = {
+  person: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    handle: PropTypes.string,
+    avatar: PropTypes.string,
+    bio: PropTypes.arrayOf(PropTypes.string)
+  })
 };
 
 export default Profile;
--- src/components/about/profile_data.js
+++ src/components/about/profile_data.js
@@ -2,12 +2,10 @@ export default [
   {
     name: 'Mike Still',
     handle: 'mikestill94',
-    avatar: 'https://pbs.twimg.com/profile_images/1046809356950335489/VFs7TW0-_400x400.jpg',
-    bio: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore dolorum voluptatem facilis placeat deserunt obcaecati cupiditate eos. Laudantium consequatur doloremque magni eligendi, optio aliquam dicta porro possimus expedita! Quod, quos.']
+    avatar: 'https://pbs.twimg.com/profile_images/1046809356950335489/VFs7TW0-_400x400.jpg'
   },
   {
     name: 'Laura Ipsum',
-    handle: 'lauraipsum',
     avatar: 'https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     bio: [
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore dolorum voluptatem facilis placeat deserunt obcaecati cupiditate eos. Laudantium consequatur doloremque magni eligendi, optio aliquam dicta porro possimus expedita! Quod, quos.',
@@ -17,7 +15,6 @@ export default [
   {
     name: 'Foo Bar',
     handle: 'foobar',
-    avatar: 'https://images.pexels.com/photos/1722198/pexels-photo-1722198.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     bio: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore dolorum voluptatem facilis placeat deserunt obcaecati cupiditate eos. Laudantium consequatur doloremque magni eligendi, optio aliquam dicta porro possimus expedita! Quod, quos.']
   },
 ];
