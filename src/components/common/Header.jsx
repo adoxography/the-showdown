0fbfc91dcefa9084ec7149161686ceadfdbe86fd
Graham Still <gstill@uw.edu>
2020-01-04 21:43:35 -0600

    Evenly space header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const Header = () => (
-  <header className="font-display uppercase my-4 md:my-4">
-    <h1 className="leading-tight text-center font-hairline text-3xl md:text-4xl lg:text-6xl text-yellow-600" style={{ letterSpacing: '0.5em' }}>
-      Manitoba <br />
-      Showdown
+  <header className="font-display font-hairline my-4 md:my-4">
+    <h1 className="leading-tight uppercase text-center font-hairline text-3xl md:text-4xl lg:text-6xl text-yellow-600" style={{ letterSpacing: '0.5em' }}>
+      <span className="inline-block w-10 lg:w-20">M</span>
+      <span className="inline-block w-10 lg:w-20">a</span>
+      <span className="inline-block w-10 lg:w-20">n</span>
+      <span className="inline-block w-10 lg:w-20">i</span>
+      <span className="inline-block w-10 lg:w-20">t</span>
+      <span className="inline-block w-10 lg:w-20">o</span>
+      <span className="inline-block w-10 lg:w-20">b</span>
+      <span className="inline-block w-10 lg:w-20">a</span>
+      <br />
+      <span className="inline-block w-10 lg:w-20">S</span>
+      <span className="inline-block w-10 lg:w-20">h</span>
+      <span className="inline-block w-10 lg:w-20">o</span>
+      <span className="inline-block w-10 lg:w-20">w</span>
+      <span className="inline-block w-10 lg:w-20">d</span>
+      <span className="inline-block w-10 lg:w-20">o</span>
+      <span className="inline-block w-10 lg:w-20">w</span>
+      <span className="inline-block w-10 lg:w-20">n</span>
     </h1>
   </header>
 );
